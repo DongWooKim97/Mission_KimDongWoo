f504579b22947807b3ea8ecbca59c14c02afc85f
김동우 <nanaukim@naver.com>
2023-04-05 14:42:05 +0900

    feat: check if press delete btn, show me the fit column(LikeablePerson)

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
@@ -12,9 +12,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -58,5 +60,13 @@ public class LikeablePersonController {
         }
 
         return "usr/likeablePerson/list";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(Principal principal, @PathVariable("id") Integer id) {
+        LikeablePerson likeablePerson = this.likeablePersonService.getLikeablePerson(id);
+        System.out.println(likeablePerson);
+
+        return "redirect:/"; // 임시로 홈으로 302
     }
 }
