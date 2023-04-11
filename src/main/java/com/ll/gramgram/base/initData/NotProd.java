c8806de2b36ce598868a655a7f6095890060d94d
김동우 <nanaukim@naver.com>
2023-04-11 23:36:05 +0900

    feat: 임시 데이터 생성(멤버 10명 추가 및 각 상황에 맞게끔 연결)

--- src/main/java/com/ll/gramgram/base/initData/NotProd.java
+++ src/main/java/com/ll/gramgram/base/initData/NotProd.java
@@ -24,6 +24,16 @@ public class NotProd {
             Member memberUser2 = memberService.join("user2", "1234").getData();
             Member memberUser3 = memberService.join("user3", "1234").getData();
             Member memberUser4 = memberService.join("user4", "1234").getData();
+            //임시로 새로운 멤버 생성
+            Member memberUser5 = memberService.join("user5", "1234").getData();
+            Member memberUser6 = memberService.join("user6", "1234").getData();
+            Member memberUser7 = memberService.join("user7", "1234").getData();
+            Member memberUser8 = memberService.join("user8", "1234").getData();
+            Member memberUser9 = memberService.join("user9", "1234").getData();
+            Member memberUser10 = memberService.join("user10", "1234").getData();
+            Member memberUser11 = memberService.join("user11", "1234").getData();
+            Member memberUser12 = memberService.join("user12", "1234").getData();
+
 
             Member memberUser5ByKakao = memberService.whenSocialLogin("KAKAO", "KAKAO__2731659195").getData();
 
@@ -31,8 +41,33 @@ public class NotProd {
             instaMemberService.connect(memberUser3, "insta_user3", "W");
             instaMemberService.connect(memberUser4, "insta_user4", "M");
 
+//            임시로 새로운 인스타멤버 생성
+            instaMemberService.connect(memberUser5, "insta_user5", "M");
+            instaMemberService.connect(memberUser6, "insta_user6", "M");
+            instaMemberService.connect(memberUser7, "insta_user7", "M");
+            instaMemberService.connect(memberUser8, "insta_user8", "M");
+            instaMemberService.connect(memberUser9, "insta_user9", "M");
+            instaMemberService.connect(memberUser10, "insta_user10", "M");
+            instaMemberService.connect(memberUser11, "insta_user11", "M");
+            instaMemberService.connect(memberUser12, "insta_user12", "M");
+
+
             likeablePersonService.like(memberUser3, "insta_user4", 1);
             likeablePersonService.like(memberUser3, "insta_user100", 2);
+
+//            임시로 새로운 호감표시 생성
+            likeablePersonService.like(memberUser12, "insta_user1", 1);
+            likeablePersonService.like(memberUser12, "insta_user2", 1);
+            likeablePersonService.like(memberUser12, "insta_user3", 1);
+            likeablePersonService.like(memberUser12, "insta_user4", 1);
+            likeablePersonService.like(memberUser12, "insta_user5", 1);
+            likeablePersonService.like(memberUser12, "insta_user6", 1);
+            likeablePersonService.like(memberUser12, "insta_user7", 1);
+            likeablePersonService.like(memberUser12, "insta_user8", 1);
+            likeablePersonService.like(memberUser12, "insta_user9", 1);
+            likeablePersonService.like(memberUser12, "insta_user10", 1);
+
+
         };
     }
 }
